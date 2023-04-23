d4bb988a0f19797394d89ae25fc4917a5d042a2e
ankit-120 <roy.ankit5008@gmail.com>
2023-04-23 14:13:27 +0530

    Update User.java

--- src/main/java/com/login/entities/User.java
+++ src/main/java/com/login/entities/User.java
@@ -15,9 +15,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-
-
-    
     private String email;
     private String mobile;
     private String password;
