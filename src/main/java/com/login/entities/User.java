b38c7b80b94c5c04600a1b504998c1ed08a51555
ankit-120 <roy.ankit5008@gmail.com>
2023-04-23 14:09:22 +0530

    more space

--- src/main/java/com/login/entities/User.java
+++ src/main/java/com/login/entities/User.java
@@ -15,6 +15,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+
     
     private String email;
     private String mobile;
